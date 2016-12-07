f0535cdf05ed164f5366cfff505450fa6243fc7f
Donatas Remeika <donatas.remeika@gmail.com>
2016-12-07 13:28:05 +0200

    Benchmarking improvements

--- benchmark.sh
+++ benchmark.sh
@@ -7,4 +7,12 @@ if [ ! -f .benchmark/20news-bydate.tar.gz ]; then
     tar zxf .benchmark/20news-bydate.tar.gz -C .benchmark
 fi
 
-# Build dictionary annotator
+# Run benchmarks
+mvn clean test
+echo "Benchmarking Dictionary Annotator"
+mvn exec:java -Dbenchmark.texts=".benchmark/20news-bydate-test" -Dbenchmark.engine="tokenmill" 2> /dev/null | grep "Benchmark"
+echo "Benchmarking Ruta MARKTABLE"
+mvn exec:java -Dbenchmark.texts=".benchmark/20news-bydate-test" -Dbenchmark.engine="ruta" 2> /dev/null | grep "Benchmark"
+echo "Benchmarking DkPro dictionary-annotator"
+mvn exec:java -Dbenchmark.texts=".benchmark/20news-bydate-test" -Dbenchmark.engine="dkpro" 2> /dev/null | grep "Benchmark"
+
--- src/test/java/lt/tokenmill/uima/dictionaryannotator/benchmark/Benchmark.java
+++ src/test/java/lt/tokenmill/uima/dictionaryannotator/benchmark/Benchmark.java
@@ -5,7 +5,6 @@ import com.google.common.collect.FluentIterable;
 import com.google.common.io.Files;
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence;
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
-import de.tudarmstadt.ukp.dkpro.core.opennlp.OpenNlpSegmenter;
 import lt.tokenmill.uima.dictionaryannotator.DictionaryAnnotator;
 import lt.tokenmill.uima.dictionaryannotator.SimpleOpenNlpTokenizer;
 import lt.tokenmill.uima.dictionaryannotator.type.DictionaryEntry;
@@ -17,45 +16,62 @@ import org.apache.uima.fit.factory.AnalysisEngineFactory;
 import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.ruta.engine.RutaEngine;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 
 public class Benchmark {
 
+    private static final Logger LOG = LoggerFactory.getLogger(Benchmark.class);
+
     public static void main(String[] args) throws Exception {
         File textDirectory = new File(args[0]);
+        LOG.info("Text directory: {}", textDirectory.getAbsolutePath());
+        String engineType = args[1].toLowerCase();
+        LOG.info("Engine: {}", engineType);
         FluentIterable<File> files = Files.fileTreeTraverser()
                 .breadthFirstTraversal(textDirectory)
                 .filter(File::isFile);
 
-        AnalysisEngine engine = createDictionaryAnnotatorEngine();
-        //AnalysisEngine engine = createDkProDictionaryAnnotatorEngine();
-        //AnalysisEngine engine = createRutaMarktableEngine();
+        AnalysisEngine engine;
+        switch (engineType) {
+            case "dkpro":
+                engine = createDkProDictionaryAnnotatorEngine();
+                break;
+            case "ruta":
+                engine = createRutaMarktableEngine();
+                break;
+            default:
+                engine = createDictionaryAnnotatorEngine();
+        }
+
         JCas jcas = engine.newJCas();
 
         long millis = 0;
-        long characters = 0;
         long texts = 0;
         long annotations = 0;
+        long tokens = 0;
 
         for (File file : files) {
             String text = Files.asCharSource(file, Charsets.UTF_8).read();
-            characters += text.length();
             jcas.setDocumentText(text);
             new Sentence(jcas, 0, text.length()).addToIndexes();
             long start = System.currentTimeMillis();
             engine.process(jcas);
             millis += (System.currentTimeMillis() - start);
             annotations += JCasUtil.select(jcas, DictionaryEntry.class).size();
+            tokens += JCasUtil.select(jcas, Token.class).size();
             texts++;
             jcas.reset();
-            if (texts > 0 && texts % 500 == 0) {
-                System.out.printf("%d texts, %d annotations, %dK characters, %.2f text/sec, %.1fK chars/sec\n",
-                        texts, annotations, characters / 1000, (1000.0 * texts / millis), (1.0 * characters / millis));
+            if (texts > 0 && texts % 1000 == 0) {
+                LOG.info(String.format("%d texts, %d annotations, %d tokens, %.2f text/sec, %.2f tokens/sec",
+                        texts, annotations, tokens, (1000.0 * texts / millis), (1000.0 * tokens / millis)));
             }
         }
-        System.out.printf("%d texts, %d annotations, %dK characters, %.2f text/sec, %.1fK chars/sec",
-                texts, annotations, characters / 1000, (1000.0 * texts / millis), (1.0 * characters / millis));
+        LOG.info(String.format("%d texts, %d annotations, %d tokens, %.2f text/sec, %.2f tokens/sec",
+                texts, annotations, tokens, (1000.0 * texts / millis), (1000.0 * tokens / millis)));
+        LOG.info(String.format("Time: %.2f seconds, tokens: %d", 0.001 * millis, tokens));
     }
 
     private static AnalysisEngine createDictionaryAnnotatorEngine() throws Exception {
@@ -70,7 +86,7 @@ public class Benchmark {
                 DictionaryAnnotator.PARAM_TOKENIZER_CLASS, SimpleOpenNlpTokenizer.class.getName(),
                 DictionaryAnnotator.PARAM_ANNOTATION_TYPE, DictionaryEntry.class.getName(),
                 DictionaryAnnotator.PARAM_CSV_SEPARATOR, ";",
-                DictionaryAnnotator.PARAM_DICTIONARY_CASE_SENSITIVE, false,
+                DictionaryAnnotator.PARAM_DICTIONARY_CASE_SENSITIVE, true,
                 DictionaryAnnotator.PARAM_DICTIONARY_ACCENT_SENSITIVE, true));
         AnalysisEngine engine = AnalysisEngineFactory.createEngine(builder.createAggregateDescription());
         return engine;
@@ -79,14 +95,9 @@ public class Benchmark {
     private static AnalysisEngine createDkProDictionaryAnnotatorEngine() throws Exception {
         AggregateBuilder builder = new AggregateBuilder();
 
-        builder.add(AnalysisEngineFactory.createEngineDescription(OpenNlpSegmenter.class,
-                OpenNlpSegmenter.PARAM_VARIANT, "maxent",
-                OpenNlpSegmenter.PARAM_LANGUAGE, "en",
-                OpenNlpSegmenter.PARAM_SEGMENTATION_MODEL_LOCATION,
-                "classpath:/de/tudarmstadt/ukp/dkpro/core/opennlp/lib/sentence-en-maxent.bin",
-                OpenNlpSegmenter.PARAM_TOKENIZATION_MODEL_LOCATION,
-                "classpath:/de/tudarmstadt/ukp/dkpro/core/opennlp/lib/token-en-maxent.bin"));
-
+        builder.add(AnalysisEngineFactory.createEngineDescription(SimpleTokenizer.class,
+                UimaUtil.SENTENCE_TYPE_PARAMETER, Sentence.class.getName(),
+                UimaUtil.TOKEN_TYPE_PARAMETER, Token.class.getName()));
 
         builder.add(AnalysisEngineFactory.createEngineDescription(de.tudarmstadt.ukp.dkpro.core.dictionaryannotator.DictionaryAnnotator.class,
                 de.tudarmstadt.ukp.dkpro.core.dictionaryannotator.DictionaryAnnotator.PARAM_MODEL_LOCATION, "classpath:benchmark-dictionary.txt",
